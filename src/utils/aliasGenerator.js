018a5b67d61afc00052ae9f1a55cb907ced748ca
fogazza <vfogacacintra@gmail.com>
2025-11-06 14:40:24 -0300

    06-11-25 / refactor: Trocando versão do Gemini e testando Actions

--- src/utils/aliasGenerator.js
+++ src/utils/aliasGenerator.js
@@ -30,7 +30,7 @@ async function aliasGenerator(itemName) {
 
     try {
         const response = await ai.models.generateContent({
-            model: 'gemini-2.5-flash',
+            model: 'gemini-2.5-lite',
             contents: prompt,
             config: config,
         });
